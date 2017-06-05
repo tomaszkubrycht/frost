ccfb42fea233509e1bfe81f08048925da01979aa
tomasz kubrycht <tomasz kubrycht>
2017-06-05 22:25:11 +0200

    Dodaj pliki projektów.

---
+++ frost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace frost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
