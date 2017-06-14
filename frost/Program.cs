c58ee791789708ca961c3575b9caefc05a0f1664
tomaszkubrycht <tomasz.kubrycht@gmail.com>
2017-06-14 22:56:00 +0200

    dodanie

--- frost/Program.cs
+++ frost/Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using CoreFtp;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using CsvHelper;
 
 namespace frost
 {
     class Program
     {
+
+        public Int64 parameter;
+        public class dane {
+             record Record = new record();
+            public async void FtpString()
+            {
+                using (var ftpClient = new FtpClient(new FtpClientConfiguration
+                {
+                    Host = "ftp.kubrycht.com.pl",
+                    Username = "frost1",
+                    Password = "Frost1"
+                }))
+                {
+                    
+                    var tempFile = new FileInfo("ERICSUNIT073430.csv");
+                    await ftpClient.LoginAsync();
+                    using (var ftpReadStream = await ftpClient.OpenFileReadStreamAsync("ERICSUNIT073430.csv"))
+                    {
+                        
+                        using (var fileWriteStream = tempFile.OpenWrite())
+                        {
+                            await ftpReadStream.CopyToAsync(fileWriteStream);
+                            var parser =new  CsvParser(tempFile)
+                            
+                            // Console.Write(ft);
+                        
+                        
+                        }
+                    }
+                }
+                
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            dane data = new dane();
+            data.FtpString();
+            Console.ReadKey();
+        }
+
+        public Int64 rect()
+        {
+            parameter = 1;
+            return parameter;
+        }
+        dynamic ReadCSV(string test)
+        {
+            return test;
         }
     }
-}
+}/
---
+++ frost/record.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace frost
+{
+    class record
+    {
+        public DateTime utc { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string red { get; set; }
+        public string green { get; set; }
+        public string blue { get; set; }
+        public string white { get; set; }
+        public string infra_red { get; set; }
+}
+}
